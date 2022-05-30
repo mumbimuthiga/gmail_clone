16f3d39cd09399a50add64527bcf0f707a2a0fc8
Mumbi Muthiga <veronicmuthiga.com>
2022-05-30 16:55:46 +0300

    update the files

--- src/EmailList.js
+++ src/EmailList.js
@@ -1,7 +1,8 @@
-import { Checkbox ,Icon,IconButton } from '@material-ui/core';
-import { ArrowDropDown, ChevronLeft, ChevronRight, KeyboardHide, MoreVert, Redo, Settings } from '@material-ui/icons';
+import { Checkbox ,IconButton } from '@material-ui/core';
+import { ArrowDropDown, ChevronLeft, ChevronRight, Inbox, KeyboardHide, MoreVert, Redo, Settings } from '@material-ui/icons';
 import React from 'react';
-import './EmailList.css'
+import './EmailList.css';
+import Section from './Section';
 
 function EmailList() {
   return (
@@ -12,6 +13,8 @@ function EmailList() {
           <IconButton>
             <ArrowDropDown></ArrowDropDown>
           </IconButton>
+
+        
 
           <IconButton>
             <Redo></Redo>
@@ -42,7 +45,9 @@ function EmailList() {
         </div>
       </div>
       <div className='emailList__sections'>
-        <Section title='Primary' icon={} color='red' selected />
+        <Section title='Primary' Icon={ArrowDropDown} color='red' selected />
+        <Section title='Social' Icon={ArrowDropDown} color='red'  />
+        <Section title='Promotions' Icon={ArrowDropDown} color='red'  />
 
       </div>
 
