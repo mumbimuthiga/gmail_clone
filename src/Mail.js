051c2f4bf38d17e9676773d2b3b8178e3e184414
Mumbi Muthiga <veronicmuthiga.com>
2022-06-27 16:46:42 +0300

    Add files

--- src/Mail.js
+++ src/Mail.js
@@ -1,11 +1,12 @@
 import { Icon, IconButton } from '@material-ui/core';
-import { ArrowBack, CheckCircle, Delete, Email, Error, LabelImportant, MoveToInbox, WatchLater } from '@material-ui/icons';
+import { ArrowBack, CheckCircle, Delete, Email, Error, ExitToApp, LabelImportant, MoveToInbox, Print, UnfoldMore, WatchLater } from '@material-ui/icons';
 import React from 'react'
 import './Mail.css';
 
 function Mail() {
   return (
     <div className='mail'>
+      <div className='mail__tools'>
         <div className='mail__toolsLeft'>
           <IconButton>
             <ArrowBack></ArrowBack>
@@ -35,7 +36,19 @@ function Mail() {
 
         </div>
         <div className='mail__toolsRight'>
+          <IconButton>
+            <UnfoldMore></UnfoldMore>
+          </IconButton>
 
+          <IconButton>
+            <Print></Print>
+          </IconButton>
+
+          <IconButton>
+            <ExitToApp></ExitToApp>
+          </IconButton>
+
+        </div>
         </div>
         </div>
   )
